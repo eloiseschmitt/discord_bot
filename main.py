f3e3d6fcde0213cab787fd2916f13119aca96e41
eloiseschmitt <eschmitt18@gmail.com>
2024-09-22 14:01:50 +0200

    add message intents

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 load_dotenv()
 
 intents = discord.Intents.default()
-#intents.message_content = True
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
